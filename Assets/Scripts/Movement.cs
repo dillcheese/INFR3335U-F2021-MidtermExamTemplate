65a48be107d003da84f3b9c3811ee2c123bd517b
Jackie <jzhou39@gmail.com>
2021-10-09 14:08:26 -0400

    level goal and some more decor

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
     //variables
     private Rigidbody rb;
 
-    public float speed = 8f;
+    public float speed = 6f;
     public float lookSpeed = 5f;
+    public float coins = 0f;
+    public bool won = false;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,17 +21,20 @@ public class Movement : MonoBehaviour
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
-        //float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
 
         //rotate based on mouse input
         transform.Rotate(0f, mouseX, 0f, Space.World);
-        //transform.Rotate(-mouseY, 0f, 0f, Space.Self);
+
+        //load end scene if the player has won
+        if (won)
+        {
+            SceneManager.LoadScene("End");
+        }
     }
 
     private void FixedUpdate()
     {
         //put physics, function called every number of seconds
-
         float dirX = Input.GetAxis("Horizontal") * speed;
         float dirZ = Input.GetAxis("Vertical") * speed;
 
@@ -36,45 +42,22 @@ public class Movement : MonoBehaviour
         // makes it so that 'forward' changes based on your camera is looking
         moveDir = Camera.main.transform.TransformDirection(moveDir);
         moveDir.y = 0f;
-        //moveDir.y = rb.velocity.y;
-        //rb.AddForce(moveDir * speed);
+
         rb.velocity = moveDir;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        //Floor ground = other.gameObject.GetComponent<Floor>();
-        //if (ground)
-        //{
-        //    isGrounded = true;
-        //}
+        if (other.gameObject.tag == "Coin")
+        {
+            Debug.Log("Triggered by Enemy");
+            coins++;
+            Destroy(other.collider.gameObject);
+        }
 
-        //Winner win = other.gameObject.GetComponent<Winner>();
-        //if (win)
-        //{
-        //    won = true;
-        //    //win con screen
-        //    //from https://answers.unity.com/questions/747872/freeze-rigidbody-position-in-script.html
-        //    rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ
-        //        | RigidbodyConstraints.FreezeRotation;
-        //}
-    }
-
-    private void OnCollisionStay(Collision other)
-    {
-        //Floor ground = other.gameObject.GetComponent<Floor>();
-        //if (ground)
-        //{
-        //    isGrounded = true;
-        //}
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        //Floor ground = other.gameObject.GetComponent<Floor>();
-        //if (ground)
-        //{
-        //    isGrounded = false;
-        //}
+        if (coins >= 10f)
+        {
+            won = true;
+        }
     }
 }
